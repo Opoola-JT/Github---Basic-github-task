203b54622a49c0f863fde29807c082d2f31a001a
opoola-jt <joshtolu15@gmail.com>
2024-07-12 04:10:47 +0100

    Konga Tests and First Java script added

---
+++ Main.java
@@ -0,0 +1,9 @@
+package com.Graham;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Josh, hope this works");
+        System.out.println("Since the last one worked, i guess we can try another");
+    }
+}
